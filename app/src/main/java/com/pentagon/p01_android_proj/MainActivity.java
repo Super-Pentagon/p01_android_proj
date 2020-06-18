2865a8013c2e2beca3467c3ded1d29437fa05756
hornhuang <307975180@qq.com>
2020-06-18 10:39:32 +0800

    Initial commit

---
+++ app/src/main/java/com/pentagon/p01_android_proj/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pentagon.p01_android_proj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
