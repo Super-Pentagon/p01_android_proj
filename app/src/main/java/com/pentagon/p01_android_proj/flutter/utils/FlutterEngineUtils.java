7abd899292f80906ee9d1b99524f461bd3655d5d
hornhuang <307975180@qq.com>
2020-06-20 16:54:03 +0800

    ADD：app 模块基本框架

---
+++ app/src/main/java/com/pentagon/p01_android_proj/flutter/utils/FlutterEngineUtils.java
@@ -0,0 +1,23 @@
+package com.pentagon.p01_android_proj.flutter.utils;
+
+public final class FlutterEngineUtils {
+
+    public final static class Home {
+
+        public final static String HOME_PAGE_ROUTE = "home_page";
+
+    }
+
+    public final static class User {
+
+        public final static String REGISTER = "register";
+
+        public final static String LOGIN = "login";
+
+        public final static String RESET_PASSWORD = "reset_password";
+
+        public final static String FORGET_PASSWORD = "forget_password";
+
+    }
+
+}
