8d49a478e0dd2d4b3c198c474810b1bc3cc1befb
FishInWater-1999 <814484626@qq.com>
2020-07-05 00:03:40 +0800

    UPD：「 用户注册模块 」

---
+++ app/src/main/java/com/pentagon/p01_android_proj/login/register/RegisterResponse.java
@@ -0,0 +1,56 @@
+package com.pentagon.p01_android_proj.login.register;
+
+/**
+ * @author: Create by huangyuanhao on 2020-07-04
+ * @email: 814484626@qq.com
+ */
+public class RegisterResponse {
+
+    /**
+     * success : true
+     * code : 20000
+     * message : 成功
+     * data : {}
+     */
+
+    private boolean success;
+    private int code;
+    private String message;
+    private DataBean data;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public DataBean getData() {
+        return data;
+    }
+
+    public void setData(DataBean data) {
+        this.data = data;
+    }
+
+    public static class DataBean {
+    }
+
+}
