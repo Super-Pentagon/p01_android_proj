a3eab97882eee384985422c864b1322f63c0b9e7
FishInWater-1999 <814484626@qq.com>
2020-07-04 22:23:54 +0800

    UPD：「 接口端口替换 」

--- app/src/main/java/com/pentagon/p01_android_proj/util/http/ApiUtils.java
+++ app/src/main/java/com/pentagon/p01_android_proj/util/http/ApiUtils.java
@@ -2,11 +2,11 @@ package com.pentagon.p01_android_proj.util.http;
 
 public class ApiUtils {
 
-    public static final String BASE_URL = "http://free.shenzhuo.vip:17838/";
+    public static final String BASE_URL = "http://free.shenzhuo.vip:17962/";
 
     public static final String BUYER_URL = BASE_URL + "buyerservice/buyer/";
 
     public static final String PRODUCT_URL = BASE_URL + "userservice/product/";
-//    GET /userservice/product/getProductById/{pid}http://free.shenzhuo.vip:17838/userservice/product
+
 
 }
